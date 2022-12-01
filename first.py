263c8be3787826f4ed49ac14b4b160deb7e48406
geoffroystanger <geoffroystanger@users.noreply.github.com>
2022-12-01 17:47:34 +0100

    Add first advent day

---
+++ first.py
@@ -0,0 +1,14 @@
+file = open("temp.txt", "r")
+temp = []
+i = 0
+temp.append(0)
+for line in file:
+    if line == "" or line == "\n":
+        i += 1
+        temp.append(0)
+    else:
+        temp[i] += int(line)
+
+temp.sort(reverse=True)
+
+print(temp[0] + temp[1]+ temp[2])
